6acda710ce364d25bc2986c92daf4b938e903934
Nicolai <nicolaiplenk@icloud.com>
2025-06-17 20:00:25 +0200

    fix titles, improve layout of reservations, add nicolai as author

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -13,22 +13,19 @@ export const author = {
   email: 'bruchbuehne@gmail.com', // i.e. hola@lenesaile.com - email of the author
 }
 export const creator = {
-  name: 'Bertbold Bühne', // i.e. Lene Saile - creator's (developer) name.
-  email: 'bruchbuehne@gmail.com',
-  website: 'https://bruchbuehne.de',
-  social: 'https://instagram.com/bruchbuehne', // i.e. https://instagram.com/lenesaile - creator's social media link
+  name: 'Nicolai Plenk', // i.e. Lene Saile - creator's (developer) name.
+  email: 'nicolai@mujstan.email',
+  website: 'https://nicola.ink',
+  social: 'https://instagram.com/untitledsophisticated', // i.e. https://instagram.com/lenesaile - creator's social media link
 };
 export const pathToSvgLogo = 'src/assets/svg/misc/logo.svg'; // used for favicon generation
 export const themeColor = '#dd4462'; // used in manifest, for example primary color value
 export const themeLight = '#f8f8f8'; // used for meta tag theme-color, if light colors are prefered. best use value set for light bg
 export const themeDark = '#2e2e2e'; // used for meta tag theme-color, if dark colors are prefered. best use value set for dark bg
-export const opengraph_default = '/assets/images/template/opengraph-default.jpg'; // fallback/default meta image
-export const opengraph_default_alt =
-  "Visible content: An Eleventy starter with CUBE CSS, Cube CSS, Every Layout, Design Tokens and Tailwind for uitility classes. A workflow for building modern and resilient websites, introduced by Andy Bell's project buildexcellentwebsit.es"; // alt text for default meta image"
 export const blog = {
   // RSS feed
   name: 'Bruchbühne Tübingen',
-  description: 'Updates zu neuen Stücken, Veranstaltungen und mehr von der Bruchbühne Tübingen.',
+  description: 'Updates zu neuen Stücken der Bruchbühne Tübingen.',
   // feed links are looped over in the head. You may add more to the array.
   feedLinks: [
     {
@@ -73,7 +70,7 @@ export const themeSwitch = {
   light: 'light',
   dark: 'dark'
 };
-export const greenweb = {
+/* export const greenweb = {
   // https://carbontxt.org/
   disclosures: [
     {
@@ -83,10 +80,10 @@ export const greenweb = {
     }
   ],
   services: [{domain: 'github.com', serviceType: 'hosting-provider'}],
-};
+}; */
 export const viewRepo = {
   // this is for the view/edit on github link. The value in the package.json will be pulled in.
-  allow: false,
+  allow: true,
   infoText: 'View this page on GitHub'
 };
 export const easteregg = true;
