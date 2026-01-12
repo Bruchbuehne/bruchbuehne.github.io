f57fe3c59fef6203a42dbcda92d9fc9c3547135a
Nicolai <nicolaiplenk@icloud.com>
2026-01-12 10:43:04 +0100

    redo favicon

--- .eleventy.js
+++ .eleventy.js
@@ -70,6 +70,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/files");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/_headers");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
   eleventyConfig.addPassthroughCopy("src/assets/**/*.{jpg,jpeg,png,gif,svg,pdf}");
 
   // Collections
